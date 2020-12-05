82c1df087f335b57c8dd97d8436551f2f4f2724e
5 admin <kirkenj@outlook.com>
2020-12-05 15:53:04 +0300

    shshshhhh

--- meow228/Program.cs
+++ meow228/Program.cs
@@ -7,6 +7,10 @@ namespace meow228
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(15/7);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
